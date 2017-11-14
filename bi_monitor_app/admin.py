aaef5803f644f7e39effd38728a0ba5836e8454d
duyabo <809618694@qq.com>
2017-11-14 16:29:25 +0800

    U bi_monitor_app/models: 增加__str__方法

--- bi_monitor_app/admin.py
+++ bi_monitor_app/admin.py
@@ -6,4 +6,10 @@ from django.contrib import admin
 from models import IntervalReport
 
 # Register your models here.
-admin.site.register(IntervalReport)
+
+
+class ReportAdmin(admin.ModelAdmin):
+    fields = ['start_date', 'end_date']
+
+
+admin.site.register(IntervalReport, ReportAdmin)
--- bi_monitor_app/models.py
+++ bi_monitor_app/models.py
@@ -10,7 +10,7 @@ class IntervalReport(models.Model):
     某个时间段内，bi接口日志分析结果
     """
     id = models.IntegerField(primary_key=True)
-    start_date = models.DateField(auto_now=True)
+    start_date = models.DateField()
     end_date = models.DateField()
     analysis_type = models.IntegerField(default=0)  # 统计类别： 0 按照 http_code, 1 按照 delay_time
     analysis_key = models.CharField(max_length=20, default='')  # 可能是 200/300/404/502，也可能是 0~1s
@@ -21,6 +21,9 @@ class IntervalReport(models.Model):
 
     class Meta:
         db_table = 't_interval_report'  # 自定义表名称为 my_table
+
+    def __str__(self):
+        return self.id
 
     @staticmethod
     def select_interval_report(start_date, end_date):
