a88fb7d562b04ad78b415acfcd4394d99f2b82d1
duyabo <809618694@qq.com>
2017-11-14 14:28:12 +0800

    A bi_monitor_app/tests: 增加view测试案例

--- bi_monitor_app/tests.py
+++ bi_monitor_app/tests.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+
+
+class TestViewDetail(TestCase):
+    """
+    测试detail这个view
+    """
+    def test_no_report(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        :return:
+        """
+        response = self.client.get(reverse('detail', kwargs={'report_id': 100}))
+        self.assertEqual(response.status_code, 404)
