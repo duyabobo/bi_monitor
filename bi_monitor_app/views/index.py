4b0f24a3d4919ff68653ffcf053d97cea99d04da
duyabo <809618694@qq.com>
2017-11-18 12:46:32 +0800

    制造假数据显示

--- bi_monitor_app/views/index.py
+++ bi_monitor_app/views/index.py
@@ -14,7 +14,21 @@ def content_detail(request):
     :param request:
     :return:
     """
-    return render(request, 'report_detail_2_dime.html')
+    return render(
+        request,
+        'report_detail_2_dime.html',
+        context={
+            'table_dates': [
+                [
+                    'test_title_of_table',
+                    [
+                        ['时间', '接口名', '统计数目'],
+                        ['2017-11-01 11:01:20', '/api/dashboard?', '20233']
+                    ]
+                ]
+            ]
+        }
+    )
 
 
 def content_list(request):
@@ -23,4 +37,16 @@ def content_list(request):
     :param request:
     :return:
     """
-    return render(request, 'report_list.html')
+    return render(
+        request,
+        'report_list.html',
+        context={
+            'api_id': 'week_report_api_id',
+            'title': 'test_title',
+            'datas': [
+                [1, '2017-11-01'],
+                [2, '2017-11-08'],
+                [3, '2017-11-15']
+            ]
+        }
+    )
